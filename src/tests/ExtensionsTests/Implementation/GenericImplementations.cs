6613c197cfa0b39f8c57d552e98bdef1b133ee2d
Alexander Kislitsyn <4lex.kislitsyn@gmail.com>
2020-02-01 10:34:01 +0300

    Added tests project.

---
+++ src/tests/ExtensionsTests/Implementation/GenericImplementations.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionsTests.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionsTests.Implementation
+{
+    public class PublicGenericImplementation : IGenericSample<object>
+    {
+    }
+
+    internal class InternalGenericImplementations : IGenericSample<object>
+    {
+
+    }
+}
---
+++ src/tests/ExtensionsTests/RegisterGenericImplemantationTests.cs
@@ -0,0 +1,32 @@
+﻿using GenericDependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExtensionsTests
+{
+    public class RegisterGenericImplemantationTests
+    {
+        [Test]
+        public void RegisterNullTypeTest()
+        {
+            var services = new ServiceCollection();
+            services.AddGenericImplementations(null, ServiceLifetime.Scoped);
+            Assert.AreEqual(0, services.Count);
+        }
+        [Test]
+        public void RegisterOnlyPublicTest()
+        {
+            var services = new ServiceCollection();
+            services.AddGenericImplementations(typeof(Interfaces.IGenericSample<>), options =>
+            {
+                options.ExcludeTypes.Add(Filters.OnlyPublic);
+            });
+            Assert.AreEqual(1, services.Count);
+        }
+    }
+}
